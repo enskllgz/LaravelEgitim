a0e2657fc4f07b14b46ceba2f84f2a181f20f1d8
enskllgz <89081322+enskllgz@users.noreply.github.com>
2025-01-26 15:41:14 +0300

    Sıfırdan Laravel Eğitimi
    -Header ve Footer düzenlendi.
    -Kategori, Hakkımızda, Ürünler, İletişim sayfaları oluşturuldu.

--- eticaret/routes/web.php
+++ eticaret/routes/web.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageHomeController;
+use App\Http\Controllers\Frontend\PageHomeController;
+use App\Http\Controllers\Frontend\PageController;
 use Inertia\Inertia;
 
 Route::get('/', [PageHomeController::class, 'anasayfa'])->name('anasayfa');
+Route::get('/hakkımızda', [PageController::class, 'hakkımızda'])->name('hakkımızda');
+Route::get('/urunler', [PageController::class, 'urunler'])->name('urunler');
+Route::get('/iletisim', [PageController::class, 'iletisim'])->name('iletisim');
+Route::get('/urun/detay', [PageController::class, 'urundetay'])->name('urundetay');
+Route::get('sepet', [PageController::class, 'sepet'])->name('sepet');
+
+
+
 
 
